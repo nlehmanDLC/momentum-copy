8f051fbeb7230a4cfbc66e90f0ce74254d0333c6
nlehman06 <nlehman06@gmail.com>
2018-11-14 15:35:49 -0500

    Terms

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,14 +1,195 @@
 <template>
-    <div>
+    <div class="h-full flex flex-col">
         <heading><img src="../assets/terms_conditions.gif"></heading>
+        <div class="flex-1 flex">
+            <momentum-textiles-nav/>
+            <div class="flex-1 pt-6 overflow-y-auto leading-open">
+                <div class=" w-3/5">
+                    <div class="font-bold">Momentum Group Policies</div>
+                    To ensure accuracy when placing orders, please have your customer number or telephone number
+                    available. Specify fabric by product number, pattern name and color. All prices shown are net price and
+                    are subject to change without notice. Prices are F.O.B. warehouse from North Carolina.
+                    <div class="mt-4 font-bold">Terms and Conditions </div>
+                        <em>Terms</em><br>
+                        Regular terms are NET 30 days (date of invoice). All invoices are dated the day of shipment. To
+                        establish terms, you are required to provide the names and addresses of five trade references, your
+                        company bank account information, your resale number, and a signed credit application. If terms have
+                        not been established, orders will be handled on a Proforma or COD basis.
+
+                    <br><br>
+
+                        <em>Conditions</em><br>
+                        Red warning labels are attached to each cut and piece of fabric shipped from our warehouse. No
+                        credit will be allowed if the terms of this warning label are violated.
+
+                    <div class="mt-4 font-bold">Quality Control</div>
+                    Purchasers and workroom employees should carefully examine material before cutting to verify
+                    correct color and pattern. Variations in color cannot be avoided as dye lots often vary. If an exact
+                    color match is required, a cutting of the required color should be submitted with the order. We cannot
+                    be held responsible for color variations beyond a reasonable commercial match. On "piece" or "full roll"
+                    orders, every effort will be made to ship as closely as possible to the required yardage. However, at
+                    times, due to piece size, there may be up to a 10% overage. This is an industry standard. We are not
+                    responsible for changes in fabric or color which are due to flame-proofing chemical processes requested
+                    by the customer.
+
+                    <div class="mt-4 font-bold">Returns and Cancellations</div>
+                    No returns will be accepted without a written authorization. All claims must be made within
+                        ninety (90) days of receipt of goods. Credit will not be issued unless the fabric is received on our
+                        premises within thirty (30) days from the date of the written return authorization. Anything
+                        returned must be in sellable condition, uncut and unprocessed. We cannot accept cancellations or
+                        returns of orders for fabrics that have been phased-out, closed-out, or products treated with any
+                        special finishes such as flame proofing, paper backing, acrylic backing, vinyl or stain-repellent
+                        finishes or customs. Returns or cancellations will not be accepted on direct fabrics. Please see
+                        additional LoomSource policies below. Credit will be allowed on authorized returns as follows:
+                    <ul class="about_bullets">
+                        <li>Cuts of less than 5 yards-NO CREDIT</li>
+                        <li>Cuts of 5 or more yards-25% Restocking Charge</li>
+                        <li>Full Rolls-20% Restocking Charge</li>
+                    </ul>
+                    If your order has been cut and packed, and is either ready for shipment or has left our premises,
+                    cancellation cannot be accepted.
+
+                    <div class="mt-4 font-bold">LoomSource Policies</div>
+                        Because LoomSource yardage is woven to order:
+                    <ul class="about_bullets">
+                        <li>Large orders must be ordered in full roll increments</li>
+                        <li>Orders are subject to a 5% variance</li>
+                        <li>Cancellations or Returns are not accepted</li>
+                    </ul>
+
+                    <div class="mt-4 font-bold">Product Warranties</div>
+                    <em>Standard</em><br>
+                    All Momentum Group’s products are produced to meet or exceed ACT (Association for Contract Textiles)
+                    quality and performance standards. A continuing guarantee under the Textile Fiber Products
+                    Identification Act is filed with the Federal Trade Commission. Additional fabric finishes applied to
+                    Momentum Group products after purchase will void any Momentum Group warranty.
+                    <p class="mt-2">We guarantee our fabric will perform in accordance with generally accepted industry standards when
+                        used under normal conditions and for the purpose intended for a period of three (3) years from date
+                        of purchase. High-abrasion fabrics (greater than 50,000 double rubs), we guarantee will perform for
+                        a period of five (5) years from the date of purchase. Our EPU coated products, we guarantee will
+                        perform for a period of ten (10) years from date of purchase. Our Silica products, we guarantee will
+                        perform for a period of fifteen (15) years from date of purchase.</p>
+
+                    <p class="mt-2">In the unlikely event that one of our products does not perform to commercial standards, when
+                        specified for the purpose intended, properly installed, used in a “single-shift” environment and
+                        maintained under normal conditions using appropriate cleaning agents and methods, Momentum Group
+                        will replace said fabrics free of charge, but will not be responsible for any labor costs.</p>
+                    <p class="mt-2"><em>Coated Fabrics</em><br>
+                        Certain clothing and accessory dyes (such as those used on denim jeans) may migrate to lighter
+                        colors. This phenomenon is increased by humidity and temperature and is irreversible. Momentum Group
+                        will not assume responsibility for dye transfer caused by external contaminants.</p>
+
+                    <p class="mt-2"><em>Wallcovering</em><br>
+                        Momentum Textiles, LLC warrants to Customer that the Products sold by Momentum Textiles will be free
+                        from defects in materials and workmanship for five (5) years from the date of shipment.</p>
+
+                    <p class="mt-2">
+                    MOMENTUM TEXTILES INCLUDES AN INSTALLATION INSTRUCTION AND CARE SHEET WITH EACH ROLL OF WALLCOVERING
+                    SOLD. Warranty coverage is subject to the condition that all Momentum Textiles instructions for
+                    installation and care of the Products are followed. IF THE INSTALLATION INSTRUCTION AND CARE SHEET DOES
+                    NOT ACCOMPANY ANY PRODUCTS SOLD BY OR FOR MOMENTUM TEXTILES, LLC CONTACT MOMENTUM TEXTILES BEFORE
+                    INSTALLATION COMMENCES.</p>
+
+                    <p  class="mt-2">
+                    In the event Momentum Textiles, LLC determines that any Product supplied by Momentum Textiles does not
+                    meet any warranty, Momentum Textiles, LLC will replace each such Product or (at Momentum Textiles’
+                    option) shall refund the net purchase price (after cash and other discounts). All warranty claims are
+                    subject to prorated charges for any service rendered during the warranty period.</p>
+
+                    <p class="mt-2">
+                    No warranty claim will be allowed by Momentum Textiles with respect to any Product purchased for further
+                    processing unless Momentum Textiles is notified of any alleged defect within 30 days after delivery to
+                    Customer and Momentum Textiles is provided with reasonable opportunity to examine the Product before its
+                    use or further processing. This warranty is subject to the further conditions that Customer notifies
+                    Momentum Textiles immediately in the event any Product at any time demonstrates any defect in materials
+                    or workmanship.</p>
+
+                    <p class="mt-2">
+                    Warranty coverage does not include any defect or performance deficiency which is the direct or indirect
+                    result, in whole or in part, of accident, abuse, misuse, vandalism, or other damage or alteration of the
+                    Product(s) by persons other than Momentum Textiles, LLC employees, combining incompatible products, use
+                    of incompatible products, use of incompatible materials, accessories or supplies, fires, floods and
+                    other similar and dissimilar natural causes, damage, neglect, alteration, or any impairment of the
+                    Product(s) resulting from causes or conditions not associated with ordinary storage, handling,
+                    installation, maintenance, service, or use, or any acts, omissions, causes, or events beyond the control
+                    of Momentum Textiles, LLC. Furthermore, warranty coverage does not extend to any defect in the
+                    Product(s) caused by (a) storage, handling, installing or processing the Product(s) under environmental
+                    conditions exceeding those recommended by Momentum Textiles or generally accepted by industry for the
+                    Product(s), or (b) any storage, handling, manufacturing, installation or fabrication process used by
+                    Customer involving the Product(s), or (c) any failure to install the Products in accordance with
+                    Momentum Textiles’ instructions or recommendations or any generally accepted industry standards or
+                    practices for installation or use of wallcovering products, accessories or supplies.</p>
+
+                    <p class="mt-2">
+                    THE WARRANTY SET FORTH ABOVE IS EXPRESSLY IN LIEU OF ANY OTHER WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+                    WITHOUT LIMITATION ANY WARRANTY Of MERCHANTABILITY OR FITNESS FOR PARTICULAR PURPOSE, EVEN IF SUCH
+                    PURPOSE IS KNOWN TO MOMENTUM TEXTILES, LLC, AND IN LIEU OF ANY OTHER OBLIGATIONS OR LIABILITY ON THE
+                    PART OF MOMENTUM TEXTILES, LLC. MOMENTUM TEXTILES, LLC NEITHER ASSUMES (NOT HAS AUTHORIZED ANYONE TO
+                    ASSUME FOR IT) ANY OTHER WARRANTY OR LIABILITY IN CONNECTION WITH ITS PRODUCTS.</p>
+
+                    <p class="mt-2">
+                    CUSTOMER’S SOLE REMEDIES WITH RESPECT TO THE PRODUCTS ARE SET FORTH HEREIN. MOMENTUM TEXTILES, LLC WILL
+                    HAVE NO LIABILITY FOR ANY CONSEQUENTIAL, INCIDENTAL, OR SPECIAL DAMAGES BY REASON OF ANY ACT OR OMISSION
+                    ARISING OUT OF OR IN CONNECTION WITH ITS PRODUCTS, OR THE SALE, DELIVERY, INSTALLATION, MAINTENANCE,
+                    OPERATION, PERFORMANCE, OR USE OF ITS PRODUCTS, including (by way of example and not by way of
+                    limitation) any damages, expenses, or losses incurred by reason of loss of use, lost revenues, lost
+                    profits, damage to associated equipment or to facilities, costs of capital or labor, costs of substitute
+                    products or facilities, cost of substitute or replacement products or services, costs associated with
+                    loss of goodwill or reputation, costs associated with downtime, damages or replacement costs related to
+                    tools, building materials, furnishings, or decorations, and any similar and dissimilar damages,
+                    expenses, or losses, whether any such liability is based on contract, tort, or other legal or equitable
+                    principles. IN NO EVENT SHALL MOMENTUM TEXTILES, LLC’S LIABILITY EXCEED THE PURCHASE PRICE OF THE
+                    PRODUCTS.</p>
+                    <p class="mt-2"><em>Sina Pearson</em><br>
+                        Due to the inherent nature of acrylic fiber used in Sunbrella® patterns, pilling or fuzzing may
+                        initally occur. This may be removed with a sweater shaver. Pills will eventually break off with wear
+                        or washing.</p>
+
+                    <div class="mt-4 font-bold">Testing Information/ Certificates</div>
+                    Flame retardant certificates, as well as copies of actual test reports (i.e., abrasion, wearability,
+                    fire rating) are available, where applicable, upon request.
+
+                    <div class="mt-4 font-bold">Pattern Repeat Advisory</div>
+                    Pattern repeats will vary slightly. Those shown on this website and in our sample books are
+                        approximate only. To avoid possible shortages, allowances should be made whenever a pattern repeat
+                        is involved.
+
+                    <div class="mt-4 font-bold">Reserve Policy</div>
+                    WEEK 1 (FIRST RESERVE): Momentum Group is happy to reserve the exact amount of yardage needed
+                        for a potential order for a period of one week (7 calendar days). During this period of time, the
+                        customer does not have to give up this yardage for any reason, unless they so choose.<br>
+                        <br>
+                        WEEK 2 (SECOND RESERVE): In cases where one week is not enough time for a purchase order to take
+                        place, Momentum Group will reserve the yardage for a second week (another 7 calendar days). However,
+                        during the second reserve, if another customer has a purchase order for the same product, the first
+                        customer with the product on reserve will have 48 hours to submit a purchase order. If the first
+                        customer's purchase order is not received in within 48 hours, they will have to surrender the
+                        yardage to the second customer.
+
+                    <div class="mt-4 font-bold">Custom Terms &amp; Conditions</div>
+                Because custom products are not from our standard line, a 50% deposit is required when an order is
+                    placed.
+
+                    <div class="mt-4 font-bold">Custom Solutions</div>
+                    Please contact your sales representative for information on creating custom fabrics.
+
+                    <div class="mt-4 font-bold">Privacy Policy</div>
+                    To view Momentum Group's Privacy Policy,
+                    <a class="no-underline text-orange" href="http://www.momtex.com/downloads/MomentumTextilesPrivacyNotice.pdf" target="_blank">click here</a>.
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Heading from "@/components/Heading.vue";
+import MomentumTextilesNav from "@/components/MomentumTextilesNav.vue";
+
 export default {
   components: {
-    Heading
+    Heading,
+    MomentumTextilesNav
   }
 };
 </script>
