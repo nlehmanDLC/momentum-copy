8bad2d779b40702738ed1594ca1918f3eb2a34e6
nlehman06 <nlehman06@gmail.com>
2018-11-15 09:18:31 -0500

    Sina Pearson index and nav

---
+++ src/components/SinaPearsonNav.vue
@@ -0,0 +1,39 @@
+<template>
+    <div style="width: 172px;" class="mr-2 flex flex-col h-full justify-between">
+        <div class="flex flex-col pt-6">
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Products
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">Collections
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">Design
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">Color Stories
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Travel Journals
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Press
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Awards
+            </router-link>
+        </div>
+        <div class="flex flex-col pb-4">
+            <router-link to="/momentum_textiles" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">
+                Momentum Textiles
+            </router-link>
+            <router-link to="/loomsource" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">
+                LoomSource
+            </router-link>
+            <router-link to="/textus" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">Textus
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "sina-pearson-nav"
+};
+</script>
--- src/views/Sinapearson.vue
+++ src/views/Sinapearson.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
+    <div class="h-full flex flex-col">
         <heading>
             <template slot="brand"><img src="../assets/sinapearson_logo.gif"></template>
         </heading>
+        <div class="flex-1 flex">
+            <sina-pearson-nav/>
+            <div class="flex-1 pt-6">
+                <carousel :image-list="imageList"></carousel>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Heading from "@/components/Heading.vue";
+import SinaPearsonNav from "@/components/SinaPearsonNav.vue";
+import Carousel from "../components/Carousel";
 export default {
   components: {
-    Heading
+    Carousel,
+    Heading,
+    SinaPearsonNav
+  },
+  data() {
+    return {
+      imageList: [
+        "textus_collection_01_home.jpg",
+        "textus_collection_02_home.jpg",
+        "palm_springs_weekend_home.jpg"
+      ]
+    };
   }
 };
 </script>
