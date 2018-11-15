19107937f396608799d0350384a53e846e6cc75a
nlehman06 <nlehman06@gmail.com>
2018-11-15 09:24:43 -0500

    LoomSource index and nav

---
+++ src/components/LoomSourceNav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="width: 172px;" class="mr-2 flex flex-col h-full justify-between">
+        <div class="flex flex-col pt-6">
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Products
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                News
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                Design
+            </router-link>
+            <router-link to="/" class="no-underline text-grey-darker hover:text-orange text-xs mb-2 font-sans">
+                History
+            </router-link>
+        </div>
+        <div class="flex flex-col pb-4">
+            <router-link to="/sinapearson" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">
+                Sina Pearson
+            </router-link>
+            <router-link to="/momentum_textiles" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">
+                Momentum Textiles
+            </router-link>
+            <router-link to="/textus" class="no-underline font-sans text-grey hover:text-orange text-xs mb-2">
+                Textus
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "loom-source-nav"
+    };
+</script>
--- src/views/LoomSource.vue
+++ src/views/LoomSource.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
+    <div class="h-full flex flex-col">
         <heading>
             <template slot="brand"><img src="../assets/loomsource_logo.gif"></template>
         </heading>
+        <div class="flex-1 flex">
+            <loom-source-nav/>
+            <div class="flex-1 pt-6">
+                <carousel :image-list="imageList"></carousel>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Heading from "@/components/Heading.vue";
+import LoomSourceNav from "@/components/LoomSourceNav.vue";
+import Carousel from "../components/Carousel";
 export default {
   components: {
-    Heading
+    Carousel,
+    Heading,
+    LoomSourceNav
+  },
+  data() {
+    return {
+      imageList: [
+        "charmed_01_home.jpg",
+        "charcoal_and_stone_02_home.jpg",
+        "charcoal_and_stone_01_home.jpg"
+      ]
+    };
   }
 };
 </script>
