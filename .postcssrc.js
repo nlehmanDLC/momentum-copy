cfe1acb05787cd280ec51301099bfdf5404e4b46
nlehman06 <nlehman06@gmail.com>
2018-11-15 09:50:31 -0500

    custom tailwind extractor for purge css

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,13 +1,30 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const isDev = process.argv.indexOf("serve") !== -1;
+
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+    }
+}
+
 module.exports = {
-  plugins: [
-    require("tailwindcss")("./src/tailwind/tailwind.js"),
-    isDev
-      ? ''
-      : purgecss({
-          content: ['./src/**/*.html', './src/!**!/!*.vue', './src/!**/!*.scss']
-        }),
-    require("autoprefixer")
-  ]
+    plugins: [
+        require("tailwindcss")("./src/tailwind/tailwind.js"),
+        isDev
+            ? ""
+            : purgecss({
+                content: [
+                    "./public/**/*.html",
+                    "./src/**/*.vue",
+                    "./src/!**/!*.scss"
+                ],
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+                        extensions: ["html", "js", "vue"]
+                    }
+                ]
+            }),
+        require("autoprefixer")
+    ]
 };
